4a915c24684b20d54b751c0dabecc4027a3eabe0
evopen <520dhh@gmail.com>
2018-03-17 13:46:18 +0800

    modify variable name

--- Class/MatrixChain.cpp
+++ Class/MatrixChain.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void MatrixChain(int *matrixChain, int chainSize, int **optimumMatrix, int **breakPointMatrix) {
+void MatrixChain(int *matrixChain, int chainSize, int **complexityMatrix, int **breakPointMatrix) {
     for (int i = 1; i <= chainSize; i++) 
-        optimumMatrix[i][i] = 0;
+        complexityMatrix[i][i] = 0;
     for (int offset = 2; offset <= chainSize; offset++) {
         for (int head = 1; head <= chainSize - offset + 1; head++) {
             int tail = head + offset - 1;
-            // Initialize optimumMatrix breakpoint to the first matrix
-            optimumMatrix[head][tail] = optimumMatrix[head + 1][tail] +
+            // Initialize complexityMatrix breakpoint to the first matrix
+            complexityMatrix[head][tail] = complexityMatrix[head + 1][tail] +
                 matrixChain[head - 1] * matrixChain[head] * matrixChain[tail];
             breakPointMatrix[head][tail] = head;
             for (int breakPoint = head + 1; breakPoint < tail; breakPoint++) {
-                int optimumPoint = optimumMatrix[head][breakPoint] + optimumMatrix[breakPoint + 1][tail] +
+                int complexity = complexityMatrix[head][breakPoint] + complexityMatrix[breakPoint + 1][tail] +
                                    matrixChain[head - 1] * matrixChain[breakPoint] * matrixChain[tail];
-                if (optimumPoint < optimumMatrix[head][tail]) {
-                    optimumMatrix[head][tail] = optimumPoint;
+                if (complexity < complexityMatrix[head][tail]) {
+                    complexityMatrix[head][tail] = complexity;
                     breakPointMatrix[head][tail] = breakPoint;
                 }
             }
@@ -26,18 +26,18 @@ void MatrixChain(int *matrixChain, int chainSize, int **optimumMatrix, int **bre
 int main() {
     int matrixColumnSize[7] = {30, 35, 15, 5, 10, 20, 25};
     int matrixCount = 6;
-    int **optimumMatrix = new int *[matrixCount];
+    int **complexityMatrix = new int *[matrixCount];
     int **breakPointMatrix = new int *[matrixCount];
     for (int i = 0; i < matrixCount; i++) {
-        optimumMatrix[i] = new int[matrixCount];
+        complexityMatrix[i] = new int[matrixCount];
         breakPointMatrix[i] = new int[matrixCount];
     }
-    MatrixChain(matrixColumnSize, matrixCount, optimumMatrix, breakPointMatrix);
+    MatrixChain(matrixColumnSize, matrixCount, complexityMatrix, breakPointMatrix);
     for (int i = 1; i <= matrixCount; i++) {
         for (int tab = 1; tab < i; tab++)
             cout << "\t";
         for (int j = i; j <= matrixCount; j++) 
-            cout << optimumMatrix[i][j] << "\t";
+            cout << complexityMatrix[i][j] << "\t";
         cout << endl;
     }
     return 0;
